4b44081e0bc6d513a0bb62555702296363d2a482
alekseim <alekseymw@gmail.com>
2024-12-01 16:52:25 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,28 @@
+import kotlin.math.absoluteValue
+
 fun main() {
+
+    fun parseInput(input: List<String>) = input.flatMap { string ->
+        val (first, second) = string.split("\\s+".toRegex())
+        listOf(1 to first.toInt(), 2 to second.toInt())
+    }.sortedWith(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second })
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val list = parseInput(input)
+        return input.indices.sumOf { (list[it].second - list[input.size + it].second).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = parseInput(input)
+        return input.indices.sumOf { index ->
+            list[index].second * list.count { it.first == 2 && it.second == list[index].second }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
